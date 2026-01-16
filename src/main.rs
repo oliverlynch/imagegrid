44c1831ac59db7f415b2f3e30df9052be8b5633b
oliverlynch <oliver@millstream.com.au>
2026-01-17 01:22:47 +0900

    feat: add oklab color processing mode

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,15 @@ use std::{
     io::{Cursor, Write},
     iter::zip,
     path::Path,
+    process::exit,
+    sync::OnceLock,
 };
 
 use clap::Parser;
-use image::{DynamicImage, GenericImageView, ImageReader};
-// use oklab::{Oklab, srgb_to_oklab};
+use image::{DynamicImage, GenericImageView, ImageReader, RgbImage};
+use oklab::{Oklab, srgb_to_oklab};
 use serde::{Deserialize, Serialize};
+use tokio::task::{self};
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
@@ -30,15 +33,33 @@ struct Args {
     sampleres: u32,
 
     /// Resolution multiplier for final image
-    #[arg(short, long, default_value_t = 2)]
+    #[arg(short, long, default_value_t = 1)]
     dpr: u32,
+
+    /// Which algorithm is used to assign thumbnails
+    #[arg(short, long, value_enum, default_value_t = DifferenceFunction::Oklab)]
+    algorithm: DifferenceFunction,
 }
+
+#[derive(Debug, Clone, clap::ValueEnum)]
+enum DifferenceFunction {
+    /// Fast
+    Rgb = 0,
+    /// Slower, More Accurate
+    Oklab,
+}
+
+// impl std::fmt::Display for DifferenceFunction {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         write!(f, "{:?}", self)
+//     }
+// }
 
 #[derive(Serialize, Deserialize)]
 struct ThumbnailData {
     path: String,
     res: u32,
-    colors: Vec<[f32; 3]>,
+    colors: Vec<[u8; 3]>,
 }
 
 #[derive(Default, Serialize, Deserialize)]
@@ -46,12 +67,20 @@ struct ThumbnailDb {
     thumbs: Vec<ThumbnailData>,
 }
 
+static INPUT_IMAGE: OnceLock<RgbImage> = OnceLock::new();
+static THUMBS_DB: OnceLock<ThumbnailDb> = OnceLock::new();
+static COMPARISON_FN: OnceLock<DifferenceFunction> = OnceLock::new();
+
+#[tokio::main]
 async fn main() {
     let args = Args::parse();
+    COMPARISON_FN.get_or_init(|| args.algorithm);
+
     println!("Targeting {}!", args.image);
 
     let mut thumbs_db = ThumbnailDb::default();
     let mut thumbs_cache: HashMap<String, DynamicImage> = HashMap::new();
+
     let thumb_data_path = Path::new(args.thumbs.as_str()).with_file_name("thumbdata");
 
     // Load thumbnail data from cache
@@ -68,7 +97,7 @@ async fn main() {
 
     let mut dirty_thumbs_db = 0u32;
 
-    let mut dir = fs::read_dir(args.thumbs)
+    let mut dir = fs::read_dir(&args.thumbs)
         .expect("to read thumbs directory")
         .into_iter();
     while let Some(Ok(thumb_entry)) = dir.next() {
@@ -87,11 +116,11 @@ async fn main() {
         {
             print!("\rProcessing new thumb {:?}", thumb_entry.path());
             std::io::stdout().flush().unwrap(); // Ensure stdout is flushed
+
             import_thumb(
                 thumb_entry.path().to_str().unwrap(),
                 args.sampleres,
-                Some(&mut thumbs_db),
-                &mut thumbs_cache,
+                &mut thumbs_db,
             )
             .expect("to import thumbnail");
             dirty_thumbs_db += 1;
@@ -110,16 +139,34 @@ async fn main() {
         );
     }
 
+    if thumbs_db.thumbs.len() < 2 {
+        eprintln!("Not enough thumbnails found in {}", &args.thumbs);
+        exit(1i32);
+    }
+
+    // Lock thumbs_db
+    THUMBS_DB.get_or_init(|| thumbs_db);
+
     // Load the target image
-    let raw_image = fs::read(&args.image).expect("to read the provided image file");
+    let raw_image = fs::read(&args.image);
+
+    if let Err(e) = raw_image {
+        eprintln!("Error loading image '{}': {}", &args.image, e);
+        exit(2i32);
+    }
+    let raw_image = raw_image.unwrap();
 
     let reader = ImageReader::new(Cursor::new(raw_image))
         .with_guessed_format()
         .expect("Cursor io never fails");
 
-    let mut output_path = args.image.clone();
-    output_path.insert_str(args.image.rfind(".").unwrap_or(args.image.len()), ".output");
-    output_path.push_str(".webp");
+    let original_path = args.image.clone();
+
+    let output_path = Path::new(&original_path);
+    let output_path = output_path
+        .parent()
+        .unwrap()
+        .join(String::from(output_path.file_stem().unwrap().to_str().unwrap()) + ".output.webp");
 
     let mut image = reader.decode().expect("to decode image");
 
@@ -136,130 +183,98 @@ async fn main() {
         crop_height,
     );
 
-    // Transform image to linear sRGB
-    // image
-    //     .apply_color_space(image::metadata::Cicp::SRGB_LINEAR, Default::default())
-    //     .expect("to transform color space");
-
     let image = image.into_rgb8();
+    INPUT_IMAGE.get_or_init(|| image);
+
     let mut target_image = image::RgbImage::new(crop_width * args.dpr, crop_height * args.dpr);
 
     let x_chunks = crop_width / args.thumbsize;
     let y_chunks = crop_height / args.thumbsize;
     let chunks = x_chunks * y_chunks;
+    let mut seen_chunks = 0u32;
 
-    // let mut chunk_refs: Vec<&ThumbnailData> = Vec::with_capacity(chunks as usize);
-    // let mut result_img = RgbImage::new(crop_width, crop_height);
+    // Create a set of tasks to process chunks async
+    let mut tasks = task::JoinSet::new();
 
     for x_chunk in 0..x_chunks {
         for y_chunk in 0..y_chunks {
-            print!("\rProcessing {}/{}", (x_chunk * y_chunks) + y_chunk, chunks);
-            std::io::stdout().flush().unwrap(); // Ensure stdout is flushed
-
-            let chunk: &DynamicImage = &image
-                .view(
-                    x_chunk * args.thumbsize,
-                    y_chunk * args.thumbsize,
-                    args.thumbsize,
-                    args.thumbsize,
-                )
-                .to_image()
-                .into();
-
-            let mut thumb = chunk
-                .thumbnail_exact(args.sampleres, args.sampleres)
-                .to_rgb8();
-            let thumb_oklab = rgb_thumb_to_oklab(&mut thumb);
-
-            let mut best_score: f32 = f32::MAX;
-            let mut best_match: Option<&ThumbnailData> = None;
-
-            for ref_thumb in &mut thumbs_db.thumbs {
-                let score = compare_thumbs(&thumb_oklab, &ref_thumb.colors);
-
-                if score < best_score {
-                    best_score = score;
-                    best_match = Some(ref_thumb);
-                }
-            }
-
-            if let Some(best) = best_match {
-                if !thumbs_cache.contains_key(&best.path) {
-                    import_thumb(&best.path, args.sampleres, None, &mut thumbs_cache)
-                        .expect("to import thumb");
-                }
-
-                let best_image = thumbs_cache
-                    .get(&best.path)
+            tasks.spawn(async move {
+                // Do async work
+                let chunk: &RgbImage = &INPUT_IMAGE
+                    .get()
                     .unwrap()
-                    .resize(
-                        args.thumbsize * args.dpr,
-                        args.thumbsize * args.dpr,
-                        image::imageops::FilterType::CatmullRom,
+                    .view(
+                        x_chunk * args.thumbsize,
+                        y_chunk * args.thumbsize,
+                        args.thumbsize,
+                        args.thumbsize,
                     )
-                    .to_rgb8();
+                    .to_image();
 
-                let x = (x_chunk * args.thumbsize * args.dpr) as i64;
-                let y = (y_chunk * args.thumbsize * args.dpr) as i64;
-
-                image::imageops::overlay(&mut target_image, &best_image, x, y);
-
-                println!(
-                    " - Overlayed {:?} with score {} at {} / {}",
-                    Path::new(&best.path).file_name().unwrap(),
-                    best_score,
-                    x,
-                    y
+                return (
+                    x_chunk,
+                    y_chunk,
+                    process_chunk(chunk, args.sampleres, THUMBS_DB.get().unwrap())
+                        .await
+                        .unwrap(),
                 );
-            }
+            });
         }
     }
-    println!("\rProcessing ............ Done!\n");
 
-    // for chunk in chunk_refs {
-    //     println!("{}", chunk.path);
-    // }
+    while let Some(res) = tasks.join_next().await {
+        let (x, y, best) = res.expect("thread failed :(");
 
-    // image.to_color_space(image::metadata::Cicp::SRGB, Default::default());
+        if !thumbs_cache.contains_key(&best.path) {
+            let image = load_image(&best.path).resize_exact(
+                args.thumbsize * args.dpr,
+                args.thumbsize * args.dpr,
+                image::imageops::FilterType::CatmullRom,
+            );
+            thumbs_cache.insert(best.path.clone(), image);
+        }
 
-    // image
-    //     .apply_color_space(image::metadata::Cicp::SRGB, Default::default())
-    //     .expect("to transform output image color space");
+        let best_image = thumbs_cache.get(&best.path).unwrap().to_rgb8();
+
+        let x = (x * args.thumbsize * args.dpr) as i64;
+        let y = (y * args.thumbsize * args.dpr) as i64;
+
+        image::imageops::overlay(&mut target_image, &best_image, x, y);
+
+        seen_chunks += 1;
+        print!("\rProcessing {}/{}", seen_chunks, chunks);
+        std::io::stdout().flush().unwrap(); // Ensure stdout is flushed
+    }
+
+    println!("\rProcessing ............ Done!");
+    print!("Saving Image...\r");
 
     target_image
         .save_with_format(&output_path, image::ImageFormat::WebP)
         .expect("to save output image");
+
+    println!("Saved image to {}", &output_path.display());
 }
 
-fn import_thumb<P>(
-    p: P,
-    res: u32,
-    thumbs_db: Option<&mut ThumbnailDb>,
-    thumbs_cache: &mut HashMap<String, DynamicImage>,
-) -> Result<(), ()>
+fn import_thumb<P>(p: P, res: u32, thumbs_db: &mut ThumbnailDb) -> Result<(), ()>
 where
     P: AsRef<std::path::Path> + Into<String>,
 {
     let image = load_image(&p);
-    let mut thumb_image = get_thumb(&image, res);
+    let thumb_image = get_thumb(&image, res);
 
     let path = String::from(p.into());
 
-    if let Some(tdb) = thumbs_db {
-        tdb.thumbs.push(ThumbnailData {
-            path: path.clone(),
-            res,
-            colors: rgb_thumb_to_oklab(&mut thumb_image),
-        });
-    }
+    thumbs_db.thumbs.push(ThumbnailData {
+        path: path.clone(),
+        res,
+        colors: rgb_thumb_to_pixels(&thumb_image),
+    });
 
-    thumbs_cache.insert(path, image);
     Ok(())
 }
 
 fn get_thumb(image: &DynamicImage, res: u32) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
-    // image.clone().thumbnail_exact(res, res).to_rgb8()
-
     image
         .clone()
         .resize_exact(res, res, image::imageops::FilterType::CatmullRom)
@@ -277,35 +292,101 @@ where
         .expect("Cursor io never fails");
 
     let image = reader.decode().expect("to decode image");
-    // image
-    //     .apply_color_space(image::metadata::Cicp::SRGB_LINEAR, Default::default())
-    //     .expect("to transform color space");
 
     image
 }
 
-fn rgb_thumb_to_oklab(thumb: &mut image::RgbImage) -> Vec<[f32; 3]> {
-    // Vec::from_iter(thumb.enumerate_pixels_mut().map(|(_x, _y, pixel)| {
-    //     let Oklab { l, a, b } = srgb_to_oklab(oklab::Rgb::from(pixel.0));
-    //     [l, a, b]
-    // }))
-
-    Vec::from_iter(thumb.enumerate_pixels_mut().map(|(_x, _y, pixel)| {
-        // let Oklab { l, a, b } = srgb_to_oklab(oklab::Rgb::from(pixel.0));
-        [pixel.0[0] as f32, pixel.0[1] as f32, pixel.0[2] as f32]
-    }))
+fn rgb_thumb_to_pixels(thumb: &RgbImage) -> Vec<[u8; 3]> {
+    Vec::from_iter(thumb.enumerate_pixels().map(|(_x, _y, pixel)| pixel.0))
 }
 
-fn compare_thumbs(a: &Vec<[f32; 3]>, b: &Vec<[f32; 3]>) -> f32 {
+fn compare_thumbs_u8(a: &Vec<[u8; 3]>, b: &Vec<[u8; 3]>) -> i32 {
+    if !a.len() == b.len() {
+        return i32::max_value();
+    }
+
+    let mut diff = 0i32;
+
+    for (x, y) in zip(a, b) {
+        diff += (x[0] as i32 - y[0] as i32).pow(2u32)
+            + (x[1] as i32 - y[1] as i32).pow(2u32)
+            + (x[2] as i32 - y[2] as i32).pow(2u32);
+    }
+
+    diff
+}
+
+fn compare_thumbs_f32(a: &Vec<[f32; 3]>, b: &Vec<[f32; 3]>) -> f32 {
     if !a.len() == b.len() {
         return f32::MAX;
     }
 
-    let mut diff: f32 = 0f32;
+    let mut diff = 0f32;
 
     for (x, y) in zip(a, b) {
-        diff += (x[0] - y[0]).powf(2f32) + (x[1] - y[1]).powf(2f32) + (x[2] - y[2]).powf(2f32);
+        diff += (x[0] - y[0]).powi(2i32) + (x[1] - y[1]).powi(2i32) + (x[2] - y[2]).powi(2i32);
     }
 
     diff
+}
+
+fn compare_thumbs_oklab(a: &Vec<[u8; 3]>, b: &Vec<[u8; 3]>) -> f32 {
+    let a_rgb = a
+        .into_iter()
+        .map(|v| lab_to_f32(srgb_to_oklab(oklab::Rgb::from(*v))));
+    let b_rgb = b
+        .into_iter()
+        .map(|v| lab_to_f32(srgb_to_oklab(oklab::Rgb::from(*v))));
+
+    compare_thumbs_f32(&a_rgb.collect(), &b_rgb.collect())
+}
+
+fn lab_to_f32(lab: Oklab) -> [f32; 3] {
+    [lab.l, lab.a, lab.b]
+}
+
+async fn process_chunk<'a>(
+    chunk: &RgbImage,
+    sampleres: u32,
+    thumbs_db: &'a ThumbnailDb,
+) -> Option<&'a ThumbnailData> {
+    let mut thumb = DynamicImage::from(chunk.clone())
+        .resize_exact(
+            sampleres,
+            sampleres,
+            image::imageops::FilterType::CatmullRom,
+        )
+        .to_rgb8();
+
+    let pixels = rgb_thumb_to_pixels(&mut thumb);
+    let mut best_match: Option<&ThumbnailData> = None;
+
+    match COMPARISON_FN.get().unwrap() {
+        DifferenceFunction::Oklab => {
+            let mut best_score = f32::MAX;
+
+            for ref_thumb in &thumbs_db.thumbs {
+                let score = compare_thumbs_oklab(&pixels, &ref_thumb.colors);
+
+                if score < best_score {
+                    best_score = score;
+                    best_match = Some(ref_thumb);
+                }
+            }
+        }
+        DifferenceFunction::Rgb => {
+            let mut best_score = i32::MAX;
+
+            for ref_thumb in &thumbs_db.thumbs {
+                let score = compare_thumbs_u8(&pixels, &ref_thumb.colors);
+
+                if score < best_score {
+                    best_score = score;
+                    best_match = Some(ref_thumb);
+                }
+            }
+        }
+    }
+
+    best_match
 }
